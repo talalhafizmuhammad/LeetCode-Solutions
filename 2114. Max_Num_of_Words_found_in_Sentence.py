f2385663886586414fbd0ec298b82ae1b3cfbaa5
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-18 05:43:40 +0500

    Update 2114. Max_Num_of_Words_found_in_Sentence.py

--- 2114. Max_Num_of_Words_found_in_Sentence.py
+++ 2114. Max_Num_of_Words_found_in_Sentence.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        r = []
+        for i in sentences:
+            r.append(len(i.split()))
+        return max(r)        
+        
