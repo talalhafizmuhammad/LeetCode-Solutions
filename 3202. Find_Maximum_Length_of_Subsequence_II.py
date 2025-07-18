f5cc2dbed8ed6c901807410485d58077d51f5ebd
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-18 05:54:05 +0500

    Update 3202. Find_Maximum_Length_of_Subsequence_II.py

--- 3202. Find_Maximum_Length_of_Subsequence_II.py
+++ 3202. Find_Maximum_Length_of_Subsequence_II.py
@@ -1,1 +1,7 @@
+class Solution:
+    def maximumLength(self, a: List[int], k: int) -> int:
+        z = Counter()
+        for p in product((v%k for v in a),range(k)):
+            z[p] = z[p[::-1]]+1
 
+        return max(z.values())
