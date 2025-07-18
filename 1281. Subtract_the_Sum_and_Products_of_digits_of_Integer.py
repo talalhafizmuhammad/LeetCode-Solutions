1a2e96bd6928223223a70652976bd152d35e9191
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-18 05:50:08 +0500

    Update 1281. Subtract_the_Sum_and_Products_of_digits_of_Integer.py

--- 1281. Subtract_the_Sum_and_Products_of_digits_of_Integer.py
+++ 1281. Subtract_the_Sum_and_Products_of_digits_of_Integer.py
@@ -1,1 +1,11 @@
 
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        a = 0
+        p, s =1, 0
+        while n > 0:
+            a = n % 10
+            s += a
+            p *= a
+            n //= 10
+        return p - s
